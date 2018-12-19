faf73384bc3a25e9dff6a39a1cf615d664940c8d
Christian Renold <chregi.renold@gmail.com>
2018-12-19 10:47:22 +0100

    changed loading indicator to css implementation with font-awesome

--- src/components/layout/LoadingIndicator.js
+++ src/components/layout/LoadingIndicator.js
@@ -4,7 +4,8 @@ import {connect} from 'react-redux';
 class LoadingIndicator extends React.Component {
     render() {
         return (this.props.loading
-            ? <p className='loading'><img src='/loading.gif' alt='' className='image is-30x30'/></p> : '');
+            ? <p className='icon loading has-text-info'><i className='fas fa-spinner fa-spin'/></p>
+            : null);
     }
 }
 
