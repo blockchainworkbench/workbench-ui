93437e07efe7377431c2e18d9913f2c368d399c9
Christian Renold <chregi.renold@gmail.com>
2019-05-01 11:46:35 +0200

    minor code refactoring

--- src/components/page/elements/ScriptElement.js
+++ src/components/page/elements/ScriptElement.js
@@ -1,5 +1,8 @@
 import { getSHA256 } from '../../../lib/helpers'
 
+/**
+ * @return {null}
+ */
 export default function ScriptElement(props) {
   handleScriptContent(props.content)
   handleScriptSrc(props.element.src)
@@ -13,33 +16,28 @@ function getElementByHash(value) {
 }
 
 function handleScriptContent(content) {
-  if (content) {
-    content.forEach(itm => {
-      if (itm !== '') {
-        const element = getElementByHash(itm)
-        if (!element.exists) {
-          const script = document.createElement('script')
-          script.id = element.hash
-          script.innerHTML = itm
-          document.body.appendChild(script)
-        }
-      }
-    })
-  }
+  if (!content) return
+  content.forEach(itm => {
+    if (itm === '') return
+    const element = getElementByHash(itm)
+    if (element.exists) return
+    const script = document.createElement('script')
+    script.id = element.hash
+    script.innerHTML = itm
+    document.body.appendChild(script)
+  })
 }
 
 function handleScriptSrc(src) {
-  if (src) {
-    const element = getElementByHash(src)
-    if (!element.exists) {
-      const script = document.createElement('script')
-      script.id = element.hash
-      let srcWithRelativePathCheck = src
-      if (!src.startsWith('http')) {
-        srcWithRelativePathCheck = `${process.env.REACT_APP_JSONFEED_BASE}${src}`
-      }
-      script.src = srcWithRelativePathCheck
-      document.body.appendChild(script)
-    }
+  if (!src) return
+  const element = getElementByHash(src)
+  if (element.exists) return
+  const script = document.createElement('script')
+  script.id = element.hash
+  let srcWithRelativePathCheck = src
+  if (!src.startsWith('http')) {
+    srcWithRelativePathCheck = `${process.env.REACT_APP_JSONFEED_BASE}${src}`
   }
+  script.src = srcWithRelativePathCheck
+  document.body.appendChild(script)
 }
